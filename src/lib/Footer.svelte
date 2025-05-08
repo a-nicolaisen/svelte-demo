662384c3044fb2a3d76710286f616e39f1209b57
a-nicolaisen <nicoa3@studetn.op.ac.nz>
2025-05-08 16:17:56 +1200

    add footer component

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,15 @@
+<script>
+    let name = "Web 1"
+</script>
+
+<footer>
+    <p>Hello {name}!</p>
+</footer>
+
+<style>
+    footer {
+        background-color: rosybrown;
+        padding: 8px;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+</style>
