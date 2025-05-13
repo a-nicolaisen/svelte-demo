c1a699bb241ed95202a7d7f39319c2c4a0aac69a
a-nicolaisen <nicoa3@studetn.op.ac.nz>
2025-05-13 16:07:29 +1200

    styled the quote

--- src/lib/Quote.svelte
+++ src/lib/Quote.svelte
@@ -14,11 +14,18 @@
 <style>
     div {
         background-color: rgb(247, 227, 227);
-        padding: 3px;
+        padding: 3px dashed rgb(192, 153, 153);
+        border-radius: 3px;
         margin-bottom: 1em;
+        border: 1px ;
     }
     blockquote {
         background-color: rgb(192, 153, 153);
         padding: 0.2em;
+        border-left: 0.5em solid black;
+    }
+    blockquote + p {
+        margin-left: 3em;
+        color: darkslategrey;
     }
 </style>
