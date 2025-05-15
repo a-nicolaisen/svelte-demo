163af25d417ebb88e9d56964e655ff6d84b80e51
a-nicolaisen <nicoa3@studetn.op.ac.nz>
2025-05-15 15:25:13 +1200

    Header component

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,28 +1,8 @@
-<script></script>
+<script>
+    import Navigation from "./Navigation.svelte";
+</script>
 
-<header>
-   <nav>
-        <ul>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Contact</li>
-        </ul>
-    </nav> 
-</header>
+<h1>My Awesome Website</h1>
 
 
-<style>
-    nav {
-        background-color: #ccc;
-        color: white;
-        padding: 0.5em;
-        text-decoration: none;
-    }
-
-    nav ul li {
-        align-items: center;
-        display: inline;
-        padding-right: 1em;
-        font-size: 1.3em;
-    }
-</style>
+<style></style>
