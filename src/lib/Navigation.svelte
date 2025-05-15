132934016f7195b9585155d575004d5e1690cc89
a-nicolaisen <nicoa3@studetn.op.ac.nz>
2025-05-15 15:37:12 +1200

    added styling to nav

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -6,3 +6,17 @@
     <a href="/">Home</a>
     <a href="/about">About</a>
 </nav>
+
+<style>
+    nav {
+        background-color: lightgray;
+        padding: 1em;
+    }
+    a {
+        text-decoration: none;
+        margin-right: 2em;
+        color: white;
+        font-size: 1.2em;
+        font-weight: bold;
+    }
+</style>
