0f23b1064bcb9c46df8402bdc7b76564ea2251a7
a-nicolaisen <nicoa3@studetn.op.ac.nz>
2025-05-22 16:36:20 +1200

    fetched employee data and displayed it

---
+++ src/routes/employees/+page.js
@@ -0,0 +1,19 @@
+
+export async function load({ fetch }) {
+    const url = "https://reqres.in/api/users?page=2"
+
+    try {
+        const response = await fetch(url, {
+            headers: {
+                "x-api-key": "reqres-free-v1",
+            }
+        });
+        if (!response.ok) {
+            throw new Error(`Response Status: ${response.status}`);
+        }
+        const employees = await response.json();
+        return { employees };
+    } catch (error) {
+        console.error(error.message);
+    }
+}
---
+++ src/routes/employees/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  let { data } = $props();
+  const employeeList = data.employees.data;
+  console.log(employeeList);
+</script>
+
+{#each employeeList as person}
+
+<!--could probably better make this a component-->
+  <div class="person">
+    <h2>{person.first_name + " " + person.last_name}</h2>
+    <img src={person.avatar} alt={person.first_name} />
+    <p>{person.email}</p>
+  </div>
+{/each}
+
+<style>
+    .person {
+        border: 1px solid grey;
+        max-width: 300px;
+        padding: 1em;
+        margin: 1em;
+    }
+
+    h2 {
+        margin-top: 0;
+    }
+
+</style>
